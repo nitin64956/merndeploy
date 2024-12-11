185d801e83f02e0fd7fd8bd4aae46b4fe177b349
Nitin Verma <nitin64956@gmail.com>
2024-12-11 17:40:26 +0530

    added route

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: ["https://deploy.com"],
+        origin: [" https://merndeploy-frontend.vercel.app"],
         methods:["POST","GET"],
         credentials: true
     }
