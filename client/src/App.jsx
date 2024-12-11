8ccc3295d037730f81f1a71d53e65951b3670093
Nitin Verma <nitin64956@gmail.com>
2024-12-11 17:52:00 +0530

    path change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,11 +14,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Open/>}></Route>
-
+      
+      <Route path="/" element={<Signup/>}></Route>
       <Route path="/home" element={<Home/>}></Route>
 
-        <Route path="/register" element={<Signup/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
 
       </Routes>
--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: [" https://merndeploy-frontend.vercel.app"],
+        origin: ["https://merndeploy-frontend.vercel.app"],
         methods:["POST","GET"],
         credentials: true
     }
